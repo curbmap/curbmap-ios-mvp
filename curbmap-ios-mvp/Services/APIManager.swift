10adedacb447b21422cb9e344936626b82a203c5
Peter Wu <petey888@gmail.com>
2018-04-24 15:01:39 -0700

    implemented logic to pass location data using LocationServices.  WIP as app breaks when getting user token

--- curbmap-ios-mvp/Services/APIManager.swift
+++ curbmap-ios-mvp/Services/APIManager.swift
@@ -11,38 +11,73 @@ import UIKit
 import Alamofire
 import AlamofireImage
 import OpenLocationCode
+import MapKit
 
 class APIManager {
     
     // Singleton for calling on all APIs
     static let shared = APIManager()
     
-    func upLoadImageText(image: UIImage, callBack: @escaping (String?, Error?) -> Void) {
-//        let imageOlc =
+    struct DeviceDescription {
+        let name: String
+        let systemName: String
+        let systemVersion: String
+        let model: String
+        
+        init(name: String, systemName: String, systemVersion: String, model: String) {
+            self.name = name
+            self.systemName = systemName
+            self.systemVersion = systemVersion
+            self.model = model
+        }
+        
+        func description() -> String {
+            return "name: \(name), systemName: \(systemName), systemVersion: \(systemVersion), model: \(model)"
+        }
+    }
+    
+    func upLoadImageText(heading: CLHeading?, location: CLLocation?, deviceDescription: DeviceDescription, image: UIImage, callBack: @escaping (String?, Error?) -> Void) {
+        // Convert headings/location data to string
+        let headingInString = heading?.trueHeading.description ?? "0.0"
+        let latittudeInString = location?.coordinate.latitude.description ?? "0.0"
+        let longitudeInString = location?.coordinate.longitude.description ?? "0.0"
+        let locationInString = latittudeInString + "," + longitudeInString
+        
+        // device info
+        let deviceDescriptionInString = deviceDescription.description()
+        
+        // image data/token
         let imageData = self.processImageData(image: image)
         let token = User.currentUser.getToken()!
-        let urlString = "https://curbmap.com:50003/imageUploadText"
-//        let headers = ["Authorization": "Bearer \(User.currentUser.getToken()!)"]
         
+        let urlString = "https://curbmap.com:50003/imageUploadText"
         let headers = [
             "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Bearer \(token)"
         ]
+        
+        // date
         let dateFormatter = DateFormatter()
         dateFormatter.timeStyle = .full
         dateFormatter.dateStyle = .full
         dateFormatter.timeZone = Calendar.current.timeZone
+        let dateInString = dateFormatter.string(from: Date())
+        
+        // timezone
+        let timeZoneInString = TimeZone.current.abbreviation() ?? ""
+        
         Alamofire.upload(multipartFormData: { MultipartFormData in
             // TODO: Eli to include messages in API response on the part of multipart data that failed
             // If Image doesn't pass, error code = 500
             // all required - image is not useful without the attached data
-            MultipartFormData.append("ios".data(using: String.Encoding.utf8)!, withName: "device") // pass string of device type
+            MultipartFormData.append(deviceDescriptionInString.data(using: String.Encoding.utf8)!, withName: "device") // pass string of device type
             MultipartFormData.append(token.data(using: .utf8)!, withName: "token") // pass string of token
-            MultipartFormData.append("\(dateFormatter.string(from: Date()))".data(using: String.Encoding.utf8)!, withName: "date") // date of local user at the time the API is called
-            MultipartFormData.append((TimeZone.current.abbreviation() ?? "").data(using: String.Encoding.utf8)!, withName: "timezone") // time zone of local user
-            MultipartFormData.append("imageOLC+".data(using: String.Encoding.utf8)!, withName: "olc") // location -> string of longitude/latitude (not from picture)
-            MultipartFormData.append("0.0".data(using: String.Encoding.utf8)!, withName: "bearing") // heading, degrees from north, like compass - from CLLocation/device (not from picture)
+            MultipartFormData.append(dateInString.data(using: String.Encoding.utf8)!, withName: "date") // date of local user at the time the API is called
+            MultipartFormData.append(timeZoneInString.data(using: String.Encoding.utf8)!, withName: "timezone") // time zone of local user
+            MultipartFormData.append(locationInString.data(using: String.Encoding.utf8)!, withName: "olc") // location -> string of longitude/latitude (not from picture)
+            MultipartFormData.append(headingInString.data(using: String.Encoding.utf8)!, withName: "bearing") // heading, degrees from north, like compass - from CLLocation/device (not from picture)
             MultipartFormData.append(imageData, withName: "image", fileName: "file.jpg", mimeType: "image/jpeg")
+            
         }, usingThreshold:UInt64.init(), to: urlString, method: .post, headers: headers as! HTTPHeaders, encodingCompletion: { encodingResult in
             switch encodingResult {
             case .success(let upload, _, _):
