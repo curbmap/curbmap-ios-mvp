d0225e96ec51b928e2261142e09ee30dc5ecaa6f
Peter Wu <petey888@gmail.com>
2018-06-12 20:35:21 -0700

    added property observer to make sure locations is enabled before retrieving coordinates and headings.  Not working right now.

--- curbmap/Controllers/DisplayViewController.swift
+++ curbmap/Controllers/DisplayViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class DisplayViewController: UIViewController {
     
     @IBOutlet weak var capturedImage: UIImageView!
     @IBOutlet weak var displayMessage: UILabel!
     var image: UIImage?
-    
+    var isLocationEnabled = CLLocationManager.locationServicesEnabled(){
+        didSet {
+            if isLocationEnabled {
+                
+                currentLocation = LocationServices.currentLocation
+            } else {
+                print("unable to retrieve location")
+            }
+        }
+    }
+    var currentLocation: LocationServices?
     // MARK: - View Config
     override func viewDidLoad() {
         if let image = image {
@@ -24,7 +35,7 @@ class DisplayViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         // get location/device info of user
-        let currentLocation = LocationServices.currentLocation
+        guard let currentLocation = currentLocation else { return }
         let heading = currentLocation.getHeading()
         let location = currentLocation.getLocation()
         let currentDevice = UIDevice.current
